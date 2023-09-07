ee21f90395be0c0c18b87d7e8002730d2a48caee
fatih Küçükdingin <68949521+kdingin@users.noreply.github.com>
2023-09-07 23:59:53 +0300

    .

---
+++ ProjectManagementApp/Controllers/PersonelBilgilerisController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManagementApp.Models.DataContext;
+using ProjectManagementApp.Models.Personel;
+
+namespace ProjectManagementApp.Controllers
+{
+    public class PersonelBilgilerisController : Controller
+    {
+        private ProjeTakipDBContext db = new ProjeTakipDBContext();
+
+        // GET: PersonelBilgileris
+        public ActionResult Index()
+        {
+            return View(db.PersonelBilgileris.ToList());
+        }
+
+        // GET: PersonelBilgileris/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonelBilgileri personelBilgileri = db.PersonelBilgileris.Find(id);
+            if (personelBilgileri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personelBilgileri);
+        }
+
+        // GET: PersonelBilgileris/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonelBilgileris/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PersonelBilgileri personelBilgileri)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PersonelBilgileris.Add(personelBilgileri);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personelBilgileri);
+        }
+
+        // GET: PersonelBilgileris/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonelBilgileri personelBilgileri = db.PersonelBilgileris.Find(id);
+            if (personelBilgileri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personelBilgileri);
+        }
+
+        // POST: PersonelBilgileris/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(PersonelBilgileri personelBilgileri)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personelBilgileri).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personelBilgileri);
+        }
+
+        // GET: PersonelBilgileris/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PersonelBilgileri personelBilgileri = db.PersonelBilgileris.Find(id);
+            if (personelBilgileri == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personelBilgileri);
+        }
+
+        // POST: PersonelBilgileris/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PersonelBilgileri personelBilgileri = db.PersonelBilgileris.Find(id);
+            db.PersonelBilgileris.Remove(personelBilgileri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
